3615bca18ecd5440b15740f6ed04a55b38cb9955
DarK <darkzephanziah@gmail.com>
2026-01-30 03:26:27 +0800

    chore: remove dist from git tracking

--- dist/cache.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const cache: {
-    get<T>(key: string): T | null;
-    set<T>(key: string, value: T, ttlSeconds?: number): void;
-    delete(key: string): void;
-    clear(): void;
-};
--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { paymongo } from './server';
-export { paymongoClient } from './client';
-export { useCheck, useSubscription, refreshBilling, $refreshTrigger } from './react';
-export { createPaymongoOrganization, createSeatLimit, getOrganizationSeats } from './organization';
-export type { FeatureConfig, PlanConfig, PaymongoAutumnConfig, AttachResponse, CheckResponse, TrackResponse, UsageRecord, } from './types';
-export type { SeatConfig } from './organization';
